41bba5589283425931a6e020788fdd2198971e08
Anders Refsdal Olsen <andersro93@ros-nett.com>
2019-07-31 13:02:39 +0200

    Added some aliases for mangaging disks

--- bash/bashrc.d/02-aliases.sh
+++ bash/bashrc.d/02-aliases.sh
@@ -19,6 +19,10 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
+# Disk management
+alias df="df -x squashfs"
+alias lsblk="lsblk | grep -v loop"
+
 # Management
 alias mysql_connect='mysql --defaults-file=/etc/mysql/debian.cnf'
 
