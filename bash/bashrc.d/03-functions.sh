b0ba3e1e8c48371c77e3a113445e2eeaa7d4888c
Anders Refsdal Olsen <andersro93@ros-nett.com>
2019-07-31 09:13:56 +0200

    Added some more functions and some minor fixes

--- bash/bashrc.d/03-functions.sh
+++ bash/bashrc.d/03-functions.sh
@@ -21,3 +21,65 @@ parse_git_branch() {
 	fi
 }
 
+
+# Small helper that will update the dotfiles using git
+update_dotfiles() {
+
+    # Execute git pull in the dotfiles folder
+    git -C ~/.dotfiles pull
+}
+
+
+# Small helper that "deploys" the dotfiles configuration
+deploy_dotfiles() {
+
+    # Create directories for the config files
+    mkdir -p ~/.config
+
+    # i3 setup
+    mkdir -p ~/.config/i3
+    rm ~/.config/i3/config -f
+    ln -s ~/.dotfiles/i3/config ~/.config/i3/config
+
+    # i3blocks setup
+    mkdir -p ~/.config/i3blocks
+    rm ~/.config/i3blocks/config -f
+    ln -s ~/.dotfiles/i3blocks/config ~/.config/i3blocks/config
+    ln -s ~/.dotfiles/i3blocks/blocks ~/.config/i3blocks/blocks
+
+    # Dunst
+    mkdir -p ~/.config/dunst
+    rm ~/.config/dunst/dunstrc -f
+    ln -s ~/.dotfiles/dunst/dunstrc ~/.config/dunst/dunstrc
+
+    # Redshift
+    rm ~/.config/redshift -Rf
+    ln -s ~/.dotfiles/redshift ~/.config/redshift
+
+    # Neovim
+    rm ~/.config/nvim -Rf
+    ln -s ~/.dotfiles/nvim ~/.config/nvim
+
+    # Bash
+    rm ~/.bashrc ~/.bash_profile -f
+    ln -s ~/.dotfiles/bash/.bashrc ~/.bashrc
+    ln -s ~/.dotfiles/bash/.bash_profile ~/.bash_profile
+
+    # Compton
+    rm ~/.config/compton.conf -f
+    ln -s ~/.dotfiles/compton/compton.conf ~/.config/compton.conf
+
+    # GTK 3.0
+    mkdir -p ~/.config/gtk-3.0
+    rm ~/.config/gtk-3.0/settings.ini -f
+    ln -s ~/.dotfiles/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+
+    # Xresources
+    rm ~/.Xresources -f
+    ln -s ~/.dotfiles/xresources/.Xresources ~/.Xresources
+
+    # Xinit
+    rm ~/.xinitrc -f
+    ln -s ~/.dotfiles/xinit/.xinitrc ~/.xinitrc
+}
+
