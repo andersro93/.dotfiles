d3fbb276cf058475c7a71277be6970eb3bf4cef7
Anders Refsdal Olsen <andersro93@gmail.com>
2025-01-17 23:34:05 +0100

    chore: add nvim config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,14 +12,28 @@ return {
       require "configs.lspconfig"
     end,
   },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+   "williamboman/mason.nvim",
+   opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "stylua",
+      "html-lsp",
+      "css-lsp",
+      "prettier",
+    },
+   },
+  },
+  {
+   "nvim-treesitter/nvim-treesitter",
+   opts = {
+    ensure_installed = {
+      "vim",
+      "lua",
+      "vimdoc",
+      "html",
+      "css",
+    },
+   },
+  }
 }
